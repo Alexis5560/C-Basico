ad8e5768ebaa76773dae3833b0dfe6314df6d9ce
Alexis5560 <103531589+Alexis5560@users.noreply.github.com>
2022-04-26 16:26:23 -0500

    Update velocidad.cpp

--- velocidad.cpp
+++ velocidad.cpp
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
    int main()
    {
-   	double t,d,velocidad=d/t;
-   	
-   	printf("introduzca el tiempo");
-   	scanf("%lf",&t);
-   	printf("introduzca la distancia");
-   	scanf("%lf",&d);
-   	
-   	velocidad=d/t;
-   	printf("la velocidad es de:%lf",velocidad);
-   	
+
+    int x=1;
+    while (x=1 )
+    {
+
+        system("cls");
+        double t,d,velocidad=d/t;
+        
+        printf("\nintroduzca el tiempo: ");
+        scanf("%lf",&t);
+        printf("\nintroduzca la distancia: ");
+        scanf("%lf",&d);
+        
+        velocidad=d/t;
+        printf("\nla velocidad es de: %.2lf",velocidad);
+        getch();
+   	}
+    
    	return 0;
    }
